c57aee8c4bb1b90681e645ce4525c9e5daa64f1a
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2014-11-27 04:31:35 +0100

    Added functions to retrieve data from the datasource to EveStatic

--- eve.go
+++ eve.go
@@ -9,17 +9,43 @@ type EveStatic struct {
 	datasource Datasource
 }
 
-func NewEveStatic(dsType DatasourceType, dsSource string) *EveStatic {
+func NewEveStatic(dsType DatasourceType, dsSource string) (*EveStatic, error) {
 	eve := &EveStatic{}
 
 	switch dsType {
 	case DatasourceTypeMySql:
-		eve.datasource = datasource.NewMysqlDatasource(dsSource)
+		data, err := datasource.NewMysqlDatasource(dsSource)
+		if err != nil {
+			return nil, err
+		}
+
+		eve.datasource = data
 		break
 	case DatasourceTypeSQLite3:
-		eve.datasource = datasource.NewSqlite3Datasource(dsSource)
+		data, err := datasource.NewSqlite3Datasource(dsSource)
+		if err != nil {
+			return nil, err
+		}
+
+		eve.datasource = data
 		break
 	}
 
-	return eve
+	return eve, nil
+}
+
+func (eve *EveStatic) Close() {
+	eve.datasource.Close()
+}
+
+func (eve *EveStatic) RawQuery(query string, args ...interface{}) (interface{}, error) {
+	return eve.datasource.RawQuery(query, args)
+}
+
+func (eve *EveStatic) GetTypeNameFromID(id int) (string, error) {
+	return eve.datasource.GetTypeNameFromID(id)
+}
+
+func (eve *EveStatic) GetTypeIDFromName(name string) (int, error) {
+	return eve.datasource.GetTypeIDFromName(name)
 }
